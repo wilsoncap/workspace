898d33d63609b0b9d401788b6358157bbeda8ce1
Wilson <wiston.98@hotmail.com>
2022-04-16 21:50:19 -0500

    make it better

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>WORKSPACE PHP</title>    
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+
+      <header>            
+            <div class="row">
+              <div class="col-md-4"></div>
+              <div class="col-md-4">
+                <h1><b><p style="color:orange;">AMERICANA - PHP</p></b></h1>
+              </div>
+              <div class="col-md-4"></div>
+             </div>
+      </header>
+
+      <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+          
+        <div class="Panel Content">
+					<div class="panel-heading">
+					  <i class="bi bi-dpad-fill"></i>
+					  <span style="color:orange;"><b>&nbsp;&nbsp;LENGUAJE DE PROGRAMACIÓN IV</b></span>
+					</div>
+				</div>
+        
+
+        <div class="panel-body">
+          <div class="row">
+            <div class="col">x</div>
+            <div class="col">x</div>
+            <div class="col">x</div>
+          </div>
+        </div>
+
+        </div>
+        <div class="col-md-1"></div>
+      </div>
+
+
+      <footer>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+				    <img src="img/logo_americana.png" width="300" height="150" class="d-inline-block align-top" alt="">                
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-5"></div>
+            <div class="col-md-4">
+				<i class="bi bi-xbox"> &nbsp;&nbsp;</i><a href="https://americana.edu.co/portal/">Visitanos</a>
+			  </div>	
+            <div class="col-md-3"></div>
+        </div>
+    </footer>
+      
+
+
+
+   
+</body>
+</html>
+
+
