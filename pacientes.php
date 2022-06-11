40b6b6272901f5c14a915fb9466a794d892b4742
Wilson <wiston.98@hotmail.com>
2022-06-10 22:07:20 -0500

    metodo get y getId

---
+++ pacientes.php
@@ -0,0 +1,95 @@
+<?php
+
+require_once 'clases/respuestas.class.php';
+require_once 'clases/pacientes.class.php';
+
+$_respuestas = new respuestas;
+$_pacientes = new pacientes;
+
+
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+
+    //echo "hola get";
+    if(isset($_GET["page"])){
+        $pagina = $_GET["page"];
+        $listaPacientes = $_pacientes->listaPacientes($pagina);
+        header("Content-Type: application/json");
+        echo json_encode($listaPacientes);
+        http_response_code(200);
+    }else if(isset($_GET['id'])){
+        $pacienteid = $_GET['id'];
+        $datosPaciente = $_pacientes->obtenerPaciente($pacienteid);
+        header("Content-Type: application/json");
+        echo json_encode($datosPaciente);
+        http_response_code(200);
+    }
+    
+}else if($_SERVER['REQUEST_METHOD'] == "POST"){
+  echo "hola POSt";
+    // //recibimos los datos enviados
+    // $postBody = file_get_contents("php://input");
+    // //enviamos los datos al manejador
+    // $datosArray = $_pacientes->post($postBody);
+    // //delvovemos una respuesta 
+    //  header('Content-Type: application/json');
+    //  if(isset($datosArray["result"]["error_id"])){
+    //      $responseCode = $datosArray["result"]["error_id"];
+    //      http_response_code($responseCode);
+    //  }else{
+    //      http_response_code(200);
+    //  }
+    //  echo json_encode($datosArray);
+    
+}else if($_SERVER['REQUEST_METHOD'] == "PUT"){
+  echo "hola PUT";
+    //   //recibimos los datos enviados
+    //   $postBody = file_get_contents("php://input");
+    //   //enviamos datos al manejador
+    //   $datosArray = $_pacientes->put($postBody);
+    //     //delvovemos una respuesta 
+    //  header('Content-Type: application/json');
+    //  if(isset($datosArray["result"]["error_id"])){
+    //      $responseCode = $datosArray["result"]["error_id"];
+    //      http_response_code($responseCode);
+    //  }else{
+    //      http_response_code(200);
+    //  }
+    //  echo json_encode($datosArray);
+
+}else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+        echo "hola DElete";
+        // $headers = getallheaders();
+        // if(isset($headers["token"]) && isset($headers["pacienteId"])){
+        //     //recibimos los datos enviados por el header
+        //     $send = [
+        //         "token" => $headers["token"],
+        //         "pacienteId" =>$headers["pacienteId"]
+        //     ];
+        //     $postBody = json_encode($send);
+        // }else{
+        //     //recibimos los datos enviados
+        //     $postBody = file_get_contents("php://input");
+        // }
+        
+        // //enviamos datos al manejador
+        // $datosArray = $_pacientes->delete($postBody);
+        // //delvovemos una respuesta 
+        // header('Content-Type: application/json');
+        // if(isset($datosArray["result"]["error_id"])){
+        //     $responseCode = $datosArray["result"]["error_id"];
+        //     http_response_code($responseCode);
+        // }else{
+        //     http_response_code(200);
+        // }
+        // echo json_encode($datosArray);
+       
+
+}else{
+    header('Content-Type: application/json');
+    $datosArray = $_respuestas->error_405();
+    echo json_encode($datosArray);
+}
+
+
+
+?>
