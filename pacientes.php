08c4faa131ffcc6d9fe29f0393f0ebfa994bd47e
Wilson <wiston.98@hotmail.com>
2022-06-12 23:15:09 -0500

    Api metodo post

--- pacientes.php
+++ pacientes.php
@@ -25,20 +25,22 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
     }
     
 }else if($_SERVER['REQUEST_METHOD'] == "POST"){
-  echo "hola POSt";
+  //echo "hola POSt";
     // //recibimos los datos enviados
-    // $postBody = file_get_contents("php://input");
-    // //enviamos los datos al manejador
-    // $datosArray = $_pacientes->post($postBody);
-    // //delvovemos una respuesta 
-    //  header('Content-Type: application/json');
-    //  if(isset($datosArray["result"]["error_id"])){
-    //      $responseCode = $datosArray["result"]["error_id"];
-    //      http_response_code($responseCode);
-    //  }else{
-    //      http_response_code(200);
-    //  }
-    //  echo json_encode($datosArray);
+    $postBody = file_get_contents("php://input");
+    //enviamos los datos al manejador
+    // $datos =  json_decode($postBody);
+    // print_r($datos);
+    $datosArray = $_pacientes->post($postBody);
+    //delvovemos una respuesta 
+     header('Content-Type: application/json');
+     if(isset($datosArray["result"]["error_id"])){
+         $responseCode = $datosArray["result"]["error_id"];
+         http_response_code($responseCode);
+     }else{
+         http_response_code(200);
+     }
+     echo json_encode($datosArray);
     
 }else if($_SERVER['REQUEST_METHOD'] == "PUT"){
   echo "hola PUT";
